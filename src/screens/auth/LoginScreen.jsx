70bc80e9c7af5299475cca7795f9e2d93041ee13
JanderC <janderalexisc@gmail.com>
2025-10-17 12:10:45 -0400

    cambios

--- src/screens/auth/LoginScreen.jsx
+++ src/screens/auth/LoginScreen.jsx
@@ -90,7 +90,7 @@ const LoginScreen = () => {
                 <div className="text-center mb-4">
                   <div className="mb-3">
                     <img 
-                      src="https://s3.ezgif.com/tmp/ezgif-3ce4002f22b186.png"
+                      src="https://www.pangeatech.com.uy:85/imagenes/helados.png"
                       alt="M Helados Logo"
                       style={{
                         maxWidth: '280px',
